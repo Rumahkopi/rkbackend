2f3d0beaf4e535dc4c7cd4abdf330b86eb4be7b6
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-11-21 14:24:27 +0700

    Backend Rumah Kopi

---
+++ controller.go
@@ -0,0 +1,49 @@
+package rkbackend
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/aiteung/atdb"
+	"github.com/whatsauth/watoken"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func GCFPostHandler(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	var Response Credential
+	Response.Status = false
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var dataadmin Admin
+	err := json.NewDecoder(r.Body).Decode(&dataadmin)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+	} else {
+		if IsPasswordValid(mconn, collectionname, dataadmin) {
+			Response.Status = true
+			tokenstring, err := watoken.Encode(dataadmin.Username, os.Getenv(PASETOPRIVATEKEYENV))
+			if err != nil {
+				Response.Message = "Gagal Encode Token : " + err.Error()
+			} else {
+				Response.Message = "Selamat Datang"
+				Response.Token = tokenstring
+			}
+		} else {
+			Response.Message = "Password Salah"
+		}
+	}
+
+	return GCFReturnStruct(Response)
+}
+
+func GCFReturnStruct(DataStuct any) string {
+	jsondata, _ := json.Marshal(DataStuct)
+	return string(jsondata)
+}
+
+func InsertData(db *mongo.Database, collection string, userdata Admin) string {
+	hash, _ := HashPassword(userdata.Password)
+	userdata.Password = hash
+	atdb.InsertOneDoc(db, collection, userdata)
+	return "Username : " + userdata.Username + "\nPassword : " + userdata.Password
+}
---
+++ paseto_test.go
@@ -0,0 +1,55 @@
+package rkbackend
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aiteung/atdb"
+	"github.com/whatsauth/watoken"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// PASETO
+func TestGeneratePrivateKeyPaseto(t *testing.T) {
+	privateKey, publicKey := watoken.GenerateKey()
+	fmt.Println(privateKey)
+	fmt.Println(publicKey)
+	hasil, err := watoken.Encode("admin", privateKey)
+	fmt.Println(hasil, err)
+}
+// Hash Pass
+func TestGeneratePasswordHash(t *testing.T) {
+	password := "adminkopi"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
+
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
+}
+
+func TestHashFunction(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "proyek3")
+	var userdata Admin
+	userdata.Username = "admin"
+	userdata.Password = "adminkopi"
+
+	filter := bson.M{"username": userdata.Username}
+	res := atdb.GetOneDoc[Admin](mconn, "admin", filter)
+	fmt.Println("Mongo User Result: ", res)
+	hash, _ := HashPassword(userdata.Password)
+	fmt.Println("Hash Password : ", hash)
+	match := CheckPasswordHash(userdata.Password, res.Password)
+	fmt.Println("Match:   ", match)
+
+}
+
+func TestIsPasswordValid(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "proyek3")
+	var userdata Admin
+	userdata.Username = "admin"
+	userdata.Password = "adminkopi"
+
+	anu := IsPasswordValid(mconn, "admin", userdata)
+	fmt.Println(anu)
+}
