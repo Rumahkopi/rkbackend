61aa906a16d22095a0aa56fd8b4a63e2522403ad
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-11-21 15:56:46 +0700

    Menambah Func Insert Produk Admin

--- handler.go
+++ handler.go
@@ -48,23 +48,23 @@ func InsertData(db *mongo.Database, collection string, userdata Admin) string {
 	return "Username : " + userdata.Username + "\nPassword : " + userdata.Password
 }
 
-// func InsertProduk(Mongoenv, dbname string, r *http.Request) string {
-// 	resp := new(Credential)
-// 	userdata := new(Admin)
-// 	resp.Status = false
-// 	conn := SetConnection(Mongoenv, dbname)
+func InsertProduk(Mongoenv, dbname string, r *http.Request) string {
+	resp := new(Credential)
+	userdata := new(Admin)
+	resp.Status = false
+	conn := SetConnection(Mongoenv, dbname)
 
-// 	err := json.NewDecoder(r.Body).Decode(&userdata)
-// 	if err != nil {
-// 		resp.Message = "error parsing application/json: " + err.Error()
-// 	} else {
-// 		resp.Status = true
-// 		insertedID, err := InsertDataProduk(conn, "produk", *userdata)
-// 		if err != nil {
-// 			resp.Message = "Gagal memasukkan data ke database: " + err.Error()
-// 		} else {
-// 			resp.Message = "Berhasil Input data dengan ID: " + insertedID.Hex()
-// 		}
-// 	}
-// 	return GCFReturnStruct(resp)
-// }
+	err := json.NewDecoder(r.Body).Decode(&userdata)
+	if err != nil {
+		resp.Message = "error parsing application/json: " + err.Error()
+	} else {
+		resp.Status = true
+		insertedID, err := InsertDataProduk(conn, "produk", *userdata)
+		if err != nil {
+			resp.Message = "Gagal memasukkan data ke database: " + err.Error()
+		} else {
+			resp.Message = "Berhasil Input data dengan ID: " + insertedID.Hex()
+		}
+	}
+	return GCFReturnStruct(resp)
+}
